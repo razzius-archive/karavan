0e80789f904aeea5e26a16dd806d37d2264fdd31
Razzi <razzi53@gmail.com>
2013-09-08 04:38:57 -0400

    client re-enabled

--- karavan.py
+++ karavan.py
@@ -28,16 +28,33 @@ def call():
     )
     return jsonify({"success": True})
 
-@app.route("/response/", methods=["GET", "POST"])
-def respond():
-    return render_template("response.xml")
-
 @app.route("/client/", methods=["GET", "POST"])
 def client():
     capability = TwilioCapability(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
     capability.allow_client_outgoing(TWILIO_SID)
     token = capability.generate()
     return render_template('client.html', token=token)
+
+@app.route("/response/", methods=["GET", "POST"])
+def respond():
+    # Make the music play
+    client = TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+    client.calls.create(
+        to="+13195943124",
+        from_="+13198048229",
+        url="http://karavan.herokuapp.com/music/"
+    )
+    # Call my phone
+    response = twiml.Response()
+    with response.dial(calledId=caller_id) as r:
+        r.number("+13195943124")
+    return str(response)
+
+@app.route("/music/", methods=["GET", "POST"])
+def music():
+    response = twiml.Response()
+    response.play("http://karavan.herokuapp.com/static/mp3/indaclub.mp3")
+    return str(response)
 
 @app.route("/voice/", methods=['GET', 'POST'])
 def voice():
@@ -47,7 +64,7 @@ def voice():
         r.number(from_number)
     return str(response)
 
-@app.route("/conference", methods=["POST"])
+@app.route("/conference/", methods=["POST"])
 def conference():
     response = twiml.Response()
     with response.dial() as dial:
